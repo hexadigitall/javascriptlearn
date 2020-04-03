ee5246f88f16111d01345a95bb31cbf9b8e7a879
Dimeji Jason <hexadigitztech@gmail.com>
2020-04-03 10:53:59 +0100

    initial

---
+++ variables.js
@@ -0,0 +1,9 @@
+let x = 7;
+let y = 3;
+let z = x + y;
+console.log('Answer: ' + z);
+
+let firstName = 'Bob';
+let lastName = 'James';
+let fullName = lastName + ' ' + firstName;
+console.log('Your names are ' + fullName);
